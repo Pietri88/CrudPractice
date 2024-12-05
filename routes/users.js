7a1e154be4021e42f53b528951fd7111a4e55255
pietri88 <karol.pietras2@gmail.com>
2024-12-05 10:59:48 -0500

    32

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,7 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  res.send(users);
+  res.send(JSON.stringify({users}, null, 3));
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
@@ -51,16 +51,46 @@ router.post("/",(req,res)=>{
 });
 
 
-// PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    // Extract email parameter and find users with matching email
+    const email = req.params.email;
+    let filtered_users = users.filter((user) => user.email === email);
+    
+    if (filtered_users.length > 0) {
+        // Select the first matching user and update attributes if provided
+        let filtered_user = filtered_users[0];
+        
+         // Extract and update DOB if provided
+        
+        let DOB = req.query.DOB;    
+        if (DOB) {
+            filtered_user.DOB = DOB;
+        }
+        
+        /*
+        Include similar code here for updating other attributes as needed
+        */
+        
+        // Replace old user entry with updated user
+        users = users.filter((user) => user.email != email);
+        users.push(filtered_user);
+        
+        // Send success message indicating the user has been updated
+        res.send(`User with the email ${email} updated.`);
+    } else {
+        // Send error message if no user found
+        res.send("Unable to find user!");
+    }
 });
 
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
+  const email = req.params.email;
+
+  users = users.filter((user) => user.email != email);
+
+  res.send(`User with the email ${email} deleted.`);
   res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
 
